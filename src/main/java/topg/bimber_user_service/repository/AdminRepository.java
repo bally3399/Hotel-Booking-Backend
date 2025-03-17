76950dcdbc2ed3c351ae01fec79e1a92ffe308c4
Baliqis <sulaimabaliqis@gmail.com>
2025-03-17 15:04:03 +0100

    refactored admin servcie

--- src/main/java/topg/bimber_user_service/repository/AdminRepository.java
+++ src/main/java/topg/bimber_user_service/repository/AdminRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, String> {
 
-    Optional<Admin> findByEmail(String email);
+    Admin findByEmail(String email);
 
     Optional<Admin> findByUsername(String username);
 }
